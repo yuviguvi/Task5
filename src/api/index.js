7e0e9e53730d6da5a0cb0888493c98f64d4e6dea
yuviguvi <50619688+yuviguvi@users.noreply.github.com>
2020-07-14 13:17:51 +0530

    Create index.js

---
+++ src/api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const url = ' https://covid19.mathdro.id/api';
+
+export const FetchData = async () =>{
+    try{
+       const {data:{confirmed,recovered,deaths,lastUpdate}}=await axios.get(url);
+
+       return{confirmed,recovered,deaths,lastUpdate};
+        
+    }catch(error){
+    return error;
+    }
+}
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+  } catch (error) {
+    return error;
+  }
+}
+
